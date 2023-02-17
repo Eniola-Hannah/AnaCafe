2bd65a966820e17e5523a628651b66beba17fd0f
Eniola Hannah <heniola395@gmail.com>
2023-02-17 07:23:42 +0100

    I installed mysqlclient and migrated to sql database

--- AnaCafe/AnaCafe/settings.py
+++ AnaCafe/AnaCafe/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "AnaCafe.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myshop',
+        'NAME': 'anacafe',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
