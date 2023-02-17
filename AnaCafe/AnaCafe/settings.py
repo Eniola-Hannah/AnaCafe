fbe74ae571e579dc97da354c64e9e366eca54878
Eniola Hannah <heniola395@gmail.com>
2023-02-17 07:11:09 +0100

    I added the static file directory in settings.py

--- AnaCafe/AnaCafe/settings.py
+++ AnaCafe/AnaCafe/settings.py
@@ -142,6 +142,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  #if you don't 
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
