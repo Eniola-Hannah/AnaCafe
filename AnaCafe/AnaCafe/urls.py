202390f342a9f6c8efe44e7ab463d2ace856a4ad
Eniola Hannah <heniola395@gmail.com>
2023-03-03 01:13:05 +0100

    I inserted the url loader in index.html

--- AnaCafe/AnaCafe/urls.py
+++ AnaCafe/AnaCafe/urls.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
+# if I should comment what is below here, I won't be able to acess the admin page on my browser
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='homepage')
+    # The next line of code is what made it possible to view my template instead of djnago installation page
+    path('', TemplateView.as_view(template_name='index.html'), name='homepage'),
+    path('drinks/', TemplateView.as_view(template_name='drinks.html'), name='drinks'),
+    path('food/', TemplateView.as_view(template_name='food.html'), name='food'),
+    path('bakery/', TemplateView.as_view(template_name='bakery.html'), name='bakery'),
+    path('protein/', TemplateView.as_view(template_name='protein.html'), name='protein'),
 ]
